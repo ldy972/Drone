2396f851f34fd68f1f7cd2279fcf2d509eddbc09
Audry21 <audrykouanda21@gmail.com>
2015-11-11 15:25:24 +0100

    merge

--- Drone_Prog/DroneLift/sdk_demo/Sources/lift_land.c
+++ Drone_Prog/DroneLift/sdk_demo/Sources/lift_land.c
@@ -13,11 +13,15 @@ DEFINE_THREAD_ROUTINE( lift_land, nomParams )
 
   printf("YOLO\n");
 
-  ardrone_at_set_progress_cmd(0b11,0.5,0,0,1);
-  usleep(5000000);
-  ardrone_at_set_progress_cmd(0b00,0,0,0,0);
+  //ardrone_at_set_progress_cmd(1,0.5,1,1,1);
+  //usleep(5000000);
+  ardrone_at_set_progress_cmd(TRUE,0.0,0.0,1.0,0);
+
+
 
   usleep(5000000);
+	ardrone_at_set_progress_cmd(TRUE,0.0,0.0,0.0,1);
+usleep(5000000);
   for (i = 0; i < 10; i++) {
     ardrone_tool_set_ui_pad_start(0);
     usleep(1000);
