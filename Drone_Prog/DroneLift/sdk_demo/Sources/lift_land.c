72b1ee4f8cc120167e6588d4e5f024f50ffc7503
Joe-Chip <salaun.trk@gmail.com>
2015-11-11 17:59:50 +0100

    Tentatives de rotation

--- Drone_Prog/DroneLift/sdk_demo/Sources/lift_land.c
+++ Drone_Prog/DroneLift/sdk_demo/Sources/lift_land.c
@@ -11,15 +11,21 @@ DEFINE_THREAD_ROUTINE( lift_land, nomParams )
   for (i = 0; i < 10; i++) {
     ardrone_tool_set_ui_pad_start(1);
     usleep(1000);
+    printf("On décolle\n");
   }
 
   printf("YOLO\n");
+  usleep(10000000);
 
   // Tentative de rotation
   // TODO A faire marcher
-  ardrone_at_set_progress_cmd(0b11,0.5,0,0,1);
-  usleep(5000000);
-  ardrone_at_set_progress_cmd(0b00,0,0,0,0);
+//  for (i = 0; i < 100; i++) {
+    ardrone_at_set_progress_cmd(TRUE,0,0,0.0,0.5);
+    usleep(30000);
+  //  printf("Up %d\n", i);
+ // }
+  ardrone_at_set_progress_cmd(0,0,0,0.0,0.0);
+  printf("Stahp !\n");
 
   usleep(5000000);
   for (i = 0; i < 10; i++) {
@@ -27,5 +33,8 @@ DEFINE_THREAD_ROUTINE( lift_land, nomParams )
   // TODO Utiliser les données de navigation pour déterminer que le drone a bien aterri
     ardrone_tool_set_ui_pad_start(0);
     usleep(1000);
+    printf("Land\n");
   }
+
+  return C_OK;
 }
