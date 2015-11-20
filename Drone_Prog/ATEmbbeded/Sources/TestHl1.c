e853811a0331088e2cd74a2113a344de4e5980d6
Kevin Salaun <salaun.trk@gmail.com>
2015-11-20 16:50:46 +0100

    Edit Makefile, remove useless files and correct test

--- Drone_Prog/ATEmbbeded/Sources/TestHl1.c
+++ Drone_Prog/ATEmbbeded/Sources/TestHl1.c
@@ -1,21 +1,19 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "com.h"
-#include "udpsender.h"
+#include "udp_sender.h"
 
 int main(void)
 {
-	taking_off_AT() 
+	taking_off_AT(); 
 	sleep(6);
 	rising_AT(500);
-	sleep(2)
+	sleep(2);
 	turn_AT_Right(500) ;
 	sleep(2);
 	landing_AT();
 
-return 0;
-
-
+    return 0;
 }
 
 
