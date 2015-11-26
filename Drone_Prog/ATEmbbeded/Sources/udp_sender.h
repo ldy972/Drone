3985495285ccb36aa5032f2f713c3b3c7083766f
Joe-Chip <salaun.trk@gmail.com>
2015-11-26 14:43:04 +0100

    Add navdata socket

--- Drone_Prog/ATEmbbeded/Sources/udp_sender.h
+++ Drone_Prog/ATEmbbeded/Sources/udp_sender.h
@@ -15,16 +15,20 @@
 #else 
 #define IP_ADRESS   "192.168.1.1"   // static IP of the drone for non embedded
 #endif
-#define UDP_PORT    5556
+#define UDP_COMMANDS_PORT   5554
+#define UDP_NAVDATA_PORT    5556
 #define MAX_BUF_LEN 1024
 
 // Open a socket on port 5556 to send messages to the drone
-int initialize_socket();
+int initialize_sockets();
 
 // Send the message to the defined IP adress on port 5556
 int send_message(char* message);
 
+// Send the message to the defined IP adress on port 5554
+int send_navdata(char* message);
+
 // Close the socket
-int close_socket();
+int close_sockets();
 
 #endif //UDP_SENDER_H
