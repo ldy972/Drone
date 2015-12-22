270510608ae492184a9ec2eb496a6e881b9d6bcc
Joe-Chip <salaun.trk@gmail.com>
2015-12-22 12:42:56 +0100

    Updated navdata test program

--- Drone_Prog/ATEmbedded/Sources/test/test_navdata.c
+++ Drone_Prog/ATEmbedded/Sources/test/test_navdata.c
@@ -18,7 +18,7 @@ int main(void)
     printf("Get navdata\n");
     int go_on = 1;
 
-    while (go_on < 50) {
+    while (go_on < 1000) {
         reload_watchdog();
         //receive data 
 		printf("Navdata reset\n") ;
@@ -28,16 +28,15 @@ int main(void)
         printf("navdata_demo:\n");
         printf("\t%13d:%s\n",   navdata_struct->navdata_option.ctrl_state,             "Control State");
         printf("\t%13d:%s\n",   navdata_struct->navdata_option.vbat_flying_percentage, "battery");
-        printf("\t%13.3f:%s\n", navdata_struct->navdata_option.theta,                  "pitch angle");
-        printf("\t%13.3f:%s\n", navdata_struct->navdata_option.phi,                    "roll  angle");
-        printf("\t%13.3f:%s\n", navdata_struct->navdata_option.psi,                    "yaw   angle");
+        printf("\t%13.3f:%s\n", navdata_struct->navdata_option.theta / 1000.0,                  "pitch angle");
+        printf("\t%13.3f:%s\n", navdata_struct->navdata_option.phi / 1000.0,                    "roll  angle");
+        printf("\t%13.3f:%s\n", navdata_struct->navdata_option.psi / 1000.0,                    "yaw   angle");
         printf("\t%13d:%s\n",   navdata_struct->navdata_option.altitude,               "altitude");
         printf("\t%13.3f:%s\n", navdata_struct->navdata_option.vx,                     "estimated vx");
         printf("\t%13.3f:%s\n", navdata_struct->navdata_option.vy,                     "estimated vy");
         printf("\t%13.3f:%s\n", navdata_struct->navdata_option.vz,                     "estimated vz");
         printf("\n\n");
         go_on ++;
-        usleep(500000);       
     }
 
     printf("Close navdata socket\n");
