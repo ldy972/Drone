7a8375ea935bbaf9780c9353b53eecc97af98b3b
Audry21 <audrykouanda21@gmail.com>
2016-01-16 15:08:20 +0100

    simulation réaliste

---
+++ Drone_Prog/ATEmbedded/Sources/core/sim_real.h
@@ -0,0 +1,19 @@
+#ifndef DEF_SIM_REAL
+#define DEF_SIM_REAL
+
+#include "navdata.h"
+
+
+#define step 10
+static int data_range = 360/step;
+
+
+void init_simu(float target); // initialise les tableaux de caps et de mesure. argument : cap cible
+float sim_get_heading(void); // donne le cap actuel
+int sim_get_power(void);// donne la puissance mesurée en direction du cap actuel
+void close_simu(void); // libération mémoire
+void sim_update(void); //incrémente de 2 le rssi si on est ds la bonne direction, diminue de 2 sinon. à appeller après avoir fait avancer le drone.
+
+
+
+#endif
