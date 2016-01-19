2c83e2b0af341453315516180bd83df72155c8a3
ldy972 <ldy972@github.com>
2016-01-19 21:32:37 +0100

    petites modifs sur transcription .bin .txt(Dongle)

--- Dongle/script_grc/transcriptIQ.c
+++ Dongle/script_grc/transcriptIQ.c
@@ -1,4 +1,3 @@
-#include <complex.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,33 +7,45 @@
 // from rtl_sdr recording format -- interleaved unsigned char
 // to gqrx/gnuradio .cfile playback format -- complex64 
 
+void closing_file(FILE *infile, FILE *outfile){
+	fclose(infile) ;
+	fclose(outfile) ;
+}
 void main(int argc, char *argv[])
 {
-  int byte1, byte2;  // int -- not unsigned char -- see fgetc man page
-  float _Complex fc;
-  const size_t fc_size = sizeof(fc);
-  FILE *infile,*outfile;
-  const float scale = 1.0/128.0;
-  const char *infilename = argv[1];
-  const char *outfilename = argv[2];
-  if (argc<3){
-    printf("usage:  rtlsdr-to-gqrx infile outfile\n");
-    exit(1);
-  }
-  // printf("in= %s out= %s \n", infilename, outfilename);
-  infile=fopen(infilename,"rb");
-  outfile=fopen(outfilename,"wb");
-  if ((infile==NULL) || (outfile==NULL)){
-    printf("Error opening files\n");
-    exit(1);
-  }
-  while ((byte1=fgetc(infile)) != EOF){
-    if ((byte2=fgetc(infile)) == EOF){
-      exit(0);
-    }
-    fc = scale*(byte1-127) + I*scale*(byte2-127);
-    fwrite(&fc,fc_size,1,outfile);
-  }
-  fclose(infile);
-  fclose(outfile); 
+	int byte1, byte2;  // int -- not unsigned char -- see fgetc man page
+	float fc;
+	FILE *infile,*outfile;
+	const float scale_carre = 1.0/(128.0*128.0);
+	const char *infilename = argv[1];
+	const char *outfilename = argv[2];
+	if (argc<3){
+		printf("usage:  rtlsdr-to-gqrx infile outfile\n");
+		exit(1);
+	}
+	printf("in= %s out= %s \n", infilename, outfilename);
+	infile=fopen(infilename,"rb");
+	outfile=fopen(outfilename,"wb");
+	if ((infile==NULL) && (outfile==NULL)){
+		printf("Error opening files\n");
+		closing_file(infile,outfile);
+		exit(-10);
+	} else if (infile==NULL){
+		fclose(outfile) ;
+		printf("Error opening input file : %s\n",infilename);
+		exit(-11);
+	}else if (outfile==NULL){
+		fclose(infile) ;
+	  	printf("Error opening output file : %s\n",outfilename);
+		exit(-11);
+	}
+	while ((byte1=fgetc(infile)) != EOF){
+		if ((byte2=fgetc(infile)) == EOF){
+			closing_file(infile,outfile) ;
+			exit(0);
+		}    
+		fc = (float)((byte1-127)*(byte1-127)+(byte2-127)*(byte2-127))*scale_carre;
+		fprintf(outfile,"%2.5f\n",fc) ;
+	}
+	closing_file(infile,outfile);
 }
