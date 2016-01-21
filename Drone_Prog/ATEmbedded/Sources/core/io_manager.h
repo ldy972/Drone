502341bf797e33bfdec6f92a841301500a31110f
Gladys <gladys.sarraf19@gmail.com>
2016-01-21 11:36:17 +0100

    filter controller io_manager

--- Drone_Prog/ATEmbedded/Sources/core/io_manager.h
+++ Drone_Prog/ATEmbedded/Sources/core/io_manager.h
@@ -4,7 +4,7 @@
 #include "com_AT.h"
 #include "navdata.h"
 #include "sim_real.h"
-
+#include "shared.h"
 
 #if defined FULL_SIMU || defined POWER_SIMU
 #define SIMU
@@ -15,5 +15,7 @@ int go_forward(int power, int times, float heading);
 int orientate_drone(int power, float target_heading);
 float retrieve_heading();
 float retrieve_power();
+trajectory_measure_t get_max_measure(trajectory_measure_t *measure);
+trajectory_measure_t get_max_power_measure();
 
 #endif
